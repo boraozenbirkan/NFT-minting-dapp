49fe090dc355b6f0e54cdeec447ee3bc5b12e4ac
boraozenbirkan <b.ozenbirkan@gmail.com>
2022-10-30 22:08:04 +0300

    Payment Test-0

--- src/App.js
+++ src/App.js
@@ -242,14 +242,7 @@ function App() {
   };
 
   const publicMint = () => {
-    let cost;
-    if (data.totalSupply >= 200){
-      cost = CONFIG.WEI_COST;
-    }
-    else{
-      cost = 0;
-    }
-    
+    let cost = 0    
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
     let totalGasLimit = String(150000);
@@ -259,7 +252,7 @@ function App() {
     setClaimingNft(true);
 
     blockchain.smartContract.methods
-      .publicMint(1)
+      .releaseTotal()
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
